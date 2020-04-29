880cc07fa1af23aa3e23795e6211eef61cd6ea47
btrang@gmail.com <btrang@gmail.com>
2020-04-28 20:52:59 -0400

    1.7 unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,11 +7,17 @@ const Button = ({handleClick, text}) =>
 const Title = ({text}) => <h1>{text}</h1>    
 
 const Statistics = ({good, bad, neutral}) => {
+  const total = good + bad + neutral
+  const average = total === 0 ? 0 : ((good*1) + (neutral*0) + (bad*-1))/total
+  const positivePct = total === 0 ? 0 : (good/total) * 100.0
   return (
     <div>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {average}</div>
+      <div>positive {positivePct} %</div>
     </div>
   )
 }
