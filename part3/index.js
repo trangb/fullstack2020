44ca1c28c5dc66dbb07a76d7e045f3a41aeff8d1
btrang@gmail.com <btrang@gmail.com>
2020-05-22 15:42:37 -0400

    3.7 Phonebook backend step7

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
 
+const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
@@ -83,7 +85,6 @@ app.post('/api/persons', (request, response) => {
 
     persons = persons.concat(person)
 
-    console.log(persons)
     response.json(person)
 })
 
