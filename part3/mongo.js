384b3fe2af0f1a6c3a3bdf43ebdb8dd1386ceb68
btrang@gmail.com <btrang@gmail.com>
2020-05-24 20:19:09 -0400

    3.12 Command-line database

---
+++ part3/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 5) {
+  console.log('Please provide the password as well as the Name & Number to add ' +
+    'to DB as command-line arguments: node mongo.js <password> <name> <number>');
+  process.exit(1)
+}
+
+//set this via command line when starting node: node mongo.js <password>
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0-updxb.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+  
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: String
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 10000)
+}
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: newName,
+  number: newNumber,
+  id: generateId()
+})
+
+person.save().then(result => {
+  console.log('person saved!', result)
+  mongoose.connection.close()
+})
+
