c1c16ae7a2bbc77542fded7b471570c2be7dace3
btrang@gmail.com <btrang@gmail.com>
2020-05-25 12:25:55 -0400

    cleanup

--- part3/index.js
+++ part3/index.js
@@ -12,29 +12,6 @@ morgan.token('data', function (req, res) {
     return JSON.stringify(req.body)
 })
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
