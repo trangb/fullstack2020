9cb54db8f5cfb41bd257959f13f84d3e6341d18a
btrang@gmail.com <btrang@gmail.com>
2020-05-30 16:27:28 -0400

    4.4 / 4.5 helper functions and unit tests, step2 / step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,34 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  //non-functional
+  // let sum = 0
+  // for (let i = 0; i < blogs.length; i++) {
+  //   sum += blogs[i].likes
+  // }
+  // return sum
+
+  //functional
+  return blogs.reduce((sum, item) => sum + item.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  let fav = blogs.reduce((init, item) => {
+    if (!init) {
+      return item
+    } else {
+      return (item.likes > init.likes) ? item : init
+    }
+  }, null)
+
+  return fav === null ? null :
+    {
+      title: fav.title,
+      author: fav.author,
+      likes: fav.likes
+    }
+}
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 }
