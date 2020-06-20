61f740b6635e4945aa8a4db08b7faddb0eb04122
btrang@gmail.com <btrang@gmail.com>
2020-06-19 20:55:34 -0400

    4.19 bloglist expansion, step7

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -8,10 +9,22 @@ blogRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findOne() //finds an arbitrary document
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   let likes = body.likes
   if (!(likes)) {
