b1ae1c875b5da8ab91e4f6f86cb9b2835dbf7647
btrang@gmail.com <btrang@gmail.com>
2020-06-20 11:23:53 -0400

    4.20 bloglist expansion, step8

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -9,18 +9,10 @@ blogRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const token = request.token
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
